895e8a33b92db745bec45e3225309a91d81b8793
yesimdemir <yesimdmr000@gmail.com>
2023-05-11 15:12:37 +0300

    JS ile yol ayrimi uyg.
    JS ile if-else ornegi

---
+++ yol_ayrimi_uyg/app.js
@@ -0,0 +1,16 @@
+//YOL AYRIMI UYGULAMASI
+
+let secilenYol = prompt("Lütfen gitmek istediğiniz yolu seçiniz: ");
+
+if(secilenYol == 1){
+    alert("Seçilen yol " + secilenYol + ". yoldur.");
+}
+else if(secilenYol == 2){
+    alert("Seçilen yol " + secilenYol + ". yoldur.");
+}
+else if(secilenYol == 3){
+    alert("Seçilen yol " + secilenYol + ". yoldur.");
+}
+else{
+    alert("Seçenekler dışında bir seçim yaptınız.");
+}
